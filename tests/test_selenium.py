81c6d6e7da6512ccbf62405d2aa408fc035a2b5a
Dhaval <dhavalkoshtidk@gmail.com>
2025-03-04 13:30:13 +0100

    comments removed, and explaination added

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -2,15 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 
+# Login test 
+
 @pytest.mark.selenium
 def test_selenium():
     driver  = webdriver.Chrome()
     driver.get("https://demo.nopcommerce.com/")
     #assert driver.title == "OrangeHRM"
     driver.implicitly_wait(5)
-    #driver.find_element(By.NAME, "username").send_keys("Admin")
-    #driver.find_element(By.NAME, "password").send_keys("admin123")
-    #driver.find_element(By.TAG_NAME, 'button').click()
+    driver.find_element(By.NAME, "username").send_keys("Admin")
+    driver.find_element(By.NAME, "password").send_keys("admin123")
+    driver.find_element(By.TAG_NAME, 'button').click()
     driver.find_element(By.PARTIAL_LINK_TEXT,"Reg").click()
     act_title = driver.title
     exp_title = "OrangeHRM"
